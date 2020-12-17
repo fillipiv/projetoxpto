b5af32a26cae5be44e37f3402a792b3f526e3b51
FillipiValadares <fillipiv@gmail.com>
2020-12-17 10:13:07 -0300

    Versionamento inicial

---
+++ src/main/java/br/projetoxpto/projetoxpto/ProjetoxptoApplication.java
@@ -0,0 +1,13 @@
+package br.projetoxpto.projetoxpto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoxptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoxptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/projetoxpto/projetoxpto/ProjetoxptoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.projetoxpto.projetoxpto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoxptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
