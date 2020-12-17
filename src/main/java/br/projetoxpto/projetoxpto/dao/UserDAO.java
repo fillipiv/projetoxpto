cd27c92b86320336c9800f4320ca03a87d9ab4d4
FillipiValadares <fillipiv@gmail.com>
2020-12-17 13:11:09 -0300

    Commit historia1 finalizada

--- src/main/java/br/projetoxpto/projetoxpto/dao/UserDAO.java
+++ src/main/java/br/projetoxpto/projetoxpto/dao/UserDAO.java
@@ -1,8 +1,6 @@
 package br.projetoxpto.projetoxpto.dao;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import br.projetoxpto.projetoxpto.model.User;
 
@@ -10,13 +8,13 @@ import br.projetoxpto.projetoxpto.model.User;
 public interface UserDAO extends CrudRepository<User, Integer> {
     //public User findByEmail(String email);
 
-    public User findByEmailAndPassword(String email, String password);
+    public User findByEmailAndSenha(String email, String senha);
 
     public User findByEmailOrRacf(String email, String racf);
 
-    @Query(value = "Select new User(u.name, u.email) from User u u.id = :cod ")
-    public User buscaPorId(@Param("cod") Integer codigo);
+    /* @Query(value = "Select new User(u.name, u.email) from User u u.id = :cod ")
+    public User buscaPorId(@Param("cod") Integer codigo); */
 
-    @Query(value = "Select name, email from user where id = :cod", nativeQuery = true)
-    public Object[] buscarUsuarioPorid(@Param("cod") Integer codigo);
+    /* @Query(value = "Select name, email from user where id = :cod", nativeQuery = true)
+    public Object[] buscarUsuarioPorid(@Param("cod") Integer codigo); */
 }
